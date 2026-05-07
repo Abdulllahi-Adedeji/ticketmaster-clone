cdeb7d95ea495f02875adf0b47613054ce713977
Abdullahi Adedeji <abdullahikolawole.adedeji@student.griffith.ie>
2026-05-07 23:39:30 +0100

    added sign in form to home page

--- app/page.js
+++ app/page.js
@@ -1,66 +1,36 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
+export default function Home(){
+    return(
+      <main className="page">
+          <form className="form"> 
+            <h1>Master Of Tickets </h1>
+            <p> Book concerts, festivals and stadium events.</p>
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+              <h1> Sign In</h1>
+
+              <label>Email</label>
+              <input type="email" placeholder="Enter your email" />
+
+              <label>Password</label>
+              <input type="password" placeholder="Enter your email" />
+
+
+              {/* Sign in button */}
+              <Link href ="/signin">
+                <button>Sign In </button>
+              </Link>
+
+              <div className=" bottom-text">
+                <p>
+                  Don't have an account ? {" "}
+                  <Link href ="/signup" >
+                    Sign Up
+                  </Link>
+                </p>
+              </div>
+          </form>
       </main>
-    </div>
-  );
+    )
 }
