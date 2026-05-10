90b7a952815a8b734d008faf5cc891497bb87de3
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2026-05-10 22:16:13 +0000

    Implement settings and allow for user updating email/password

--- app/api/me/route.js
+++ app/api/me/route.js
@@ -3,26 +3,24 @@ import pool from "../../lib/db";
 
 export async function GET() {
     const cookieStore = await cookies();
-    const userID = cookieStore.get("session");
+    const session = cookieStore.get("session");
 
-    if (!userID) {
+    if (!session) {
         return Response.json({ loggedIn: false });
     }
 
-    const [rows] =await pool.execute(
-        "SELECT UserID, Username, UserType FROM Users WHERE UserID = ?",
-        [userID.value]
+    const [rows] = await pool.execute(
+        "SELECT username, email, usertype FROM Users WHERE userID = ?",
+        [session.value]
     );
 
-    if(rows.length === 0){
-        return Response.json({ loggedIn :false});
-    }
+    if (!rows.length) return Response.json({ loggedIn: false });
 
-    return Response.json({ 
-        loggedIn: true, 
-        userID: rows[0].UserID,
-        username : rows[0].Username,
-        role: rows[0].UserType,
-     });
-
+    return Response.json({
+        loggedIn: true,
+        userID: session.value,
+        name: rows[0].username,
+        email: rows[0].email,
+        usertype: rows[0].usertype,
+    });
 }
